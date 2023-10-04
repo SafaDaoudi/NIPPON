c0fec3ea45ccb98b4119e4b2080198ca5f8ab381
Mads Als Andersen <126453479+MadsAls@users.noreply.github.com>
2023-10-04 15:55:08 +0200

    Header og forside færdig 04-10 Mads

--- Kodemappe/script.js
+++ Kodemappe/script.js
@@ -1,40 +1,4 @@
-// FORSIDE
-/******* Billedkarrusel *******/
-// // Variable:
-// const førstefoto = document.getElementById("førstefoto");
-// const midterfoto = document.getElementById("midterfoto");
-// const sidstefoto = document.getElementById("sidstefoto");
-
-// const venstrepil = document.getElementById("venstrepil"); 
-// const hoejrepil = document.getElementById("højrepil");
-
-
-// // Array
-// const carousel = [førstefoto, midterfoto, sidstefoto];
-
-// carousel[0].style.display = "block";
-// carousel[1].style.display = "none";
-// carousel[2].style.display = "none";
-
-// // Knapper med billeder af pile
-// højrepil.addEventListener("click", naesteBillede);
-// venstrepil.addEventListener("click", forrigeBillede);
-
-// // Funktioner
-// function naesteBillede(){
-//     carousel[0].style.display = "none"; // Skjul den viste
-//     carousel.push(carousel[0]); // Put den første ind til sidst
-//     carousel.shift(); // Fjern den første og flyt alle en ned
-//     carousel[0].style.display = "block"; // Vis den der nu er først
-// }
-
-// function forrigeBillede(){
-//     carousel[0].style.display = "none";
-//     carousel.unshift(carousel[2]);
-//     carousel.pop();
-//     carousel[0].style.display = "block";
-// }
-
+// Chatgbt's kode
 
 const venstrepil = document.getElementById('venstrepil');
 const højrepil = document.getElementById('højrepil');
